8ec6dfecd8105a4578c0c328b78ba12cb2ac9c9e
magicHack3r <dnjsdlf61@naver.com>
2021-08-04 18:22:58 +0900

    '2021.08.04'

--- src/main/java/hello/hellospring/repository/UserRepository.java
+++ src/main/java/hello/hellospring/repository/UserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUserName(@Param("userName") String userName);
-    
+    User findByUserPassword(@Param("userPassword") String userPassword);
+    User findByUserName(@Param("userId") Long userId);
 }
